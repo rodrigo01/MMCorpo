9d0572351331e37e3f2f9ced3affdc983e89356d
Rodrigo Ornelas <mailbox@rodrigo.mx>
2017-10-25 01:43:17 -0700

    desde el ejemplo

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -31,9 +31,16 @@ var app = {
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+
+        var ref = window.open('http://apache.org', '_blank', 'location=yes');
+
+        ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+        ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
+        ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+        ref.addEventListener('exit', function(event) { alert(event.type); });
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
