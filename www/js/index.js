ad42dbbfc8c939f376d805b4739561c947d53ff5
Rodrigo Ornelas <mailbox@rodrigo.mx>
2017-10-25 01:49:13 -0700

    preparado

--- www/js/index.js
+++ www/js/index.js
@@ -35,22 +35,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
-        var ref = window.open('http://apache.org', '_blank', 'location=yes');
+        var ref = window.open('http://www.tecsolutionsmx.com/mmcorpo/', '_blank', 'location=yes');
 
-        ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
-        ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
-        ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-        ref.addEventListener('exit', function(event) { alert(event.type); });
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
