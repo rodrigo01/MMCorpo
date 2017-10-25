29f12a3931585f49310009b45cd8c473462d7e68
Rodrigo Ornelas <mailbox@rodrigo.mx>
2017-10-25 01:52:01 -0700

    sera el sitio ?

--- www/js/index.js
+++ www/js/index.js
@@ -35,7 +35,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
-        var ref = window.open('http://www.tecsolutionsmx.com/mmcorpo/', '_blank', 'location=yes');
+        var ref = window.open('http://apache.org', '_blank', 'location=yes');
 
     }
 };
